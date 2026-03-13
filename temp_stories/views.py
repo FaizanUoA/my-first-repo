c5ea3f0a66dfd3f35f19bc81fd78702b1361d664
FaizanUoA <t17mk25@abdn.ac.uk>
2026-03-13 14:44:55 +0000

    Edited HTML logic

--- temp_stories/views.py
+++ temp_stories/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 def story_page(request):
     raw_temperature = request.GET.get("temp", "").strip()
+    if raw_temperature:
+        story = f"It is {raw_temperature}C today my friends!"
 
     context = {
         "temp": raw_temperature,
         "error": None,
-        "story": None,
+        "story": story,
     }
     return render(request, "temp_stories/index.html", context)
