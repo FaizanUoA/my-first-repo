ffe44ca097bbeac253a4d9952b743d277cf09ce3
FaizanUoA <t17mk25@abdn.ac.uk>
2026-03-13 14:57:39 +0000

    Edited HTML logic

--- temp_stories/views.py
+++ temp_stories/views.py
@@ -3,12 +3,23 @@ from django.shortcuts import render
 
 def story_page(request):
     raw_temperature = request.GET.get("temp", "").strip()
+
+    error = None
+    story = None
+    temp = raw_temperature
     if raw_temperature:
-        story = f"It is {raw_temperature}C today my friends!"
+        try:
+            temp_value = float(raw_temperature)
+            if not temp_value.is_integer():
+                error = "Please enter a whole number."
+            else:
+                story = f"It is {int(temp_value)}C today my friends!"
+        except ValueError:
+            error = "Please enter a valid number."
 
     context = {
-        "temp": raw_temperature,
-        "error": None,
+        "temp": temp,
+        "error": error,
         "story": story,
     }
     return render(request, "temp_stories/index.html", context)
